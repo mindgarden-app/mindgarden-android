941b7629c88889523c17e3bf656bc8f82173110b
godjoy <tlschgml556@gmail.com>
2021-05-07 12:10:11 +0900

    [feature/main] add Main Activity * MainActivity 추가 * show / hide extension function 추가

--- app/src/main/java/com/mindgarden/mindgarden/MainActivity.kt
+++ app/src/main/java/com/mindgarden/mindgarden/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.mindgarden.mindgarden
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mindgarden.mindgarden.ext.hide
+import com.mindgarden.mindgarden.ext.show
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        settingNavigation()
+    }
+
+    private fun settingNavigation() {
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_diary, R.id.navigation_diary_list
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home, R.id.navigation_diary_list -> navView.show()
+                else -> navView.hide()
+            }
+        }
     }
 }

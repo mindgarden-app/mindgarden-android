51cbcb368acd33aa4f6c35829e44d25a302008ba
godjoy <tlschgml556@gmail.com>
2021-07-21 09:05:44 +0900

    #16 UI * Resource, view, view item 추가

--- app/src/main/java/com/mindgarden/mindgarden/util/bindingAdapter/BindingUtils.kt
+++ app/src/main/java/com/mindgarden/mindgarden/util/bindingAdapter/BindingUtils.kt
@@ -1,9 +1,18 @@
 package com.mindgarden.mindgarden.util.bindingAdapter
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter("visible")
 fun setVisible(view: View, isVisible: Boolean) {
     view.visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("bind:setDrawableRes")
+fun setImageRes(view: ImageView, drawableRes: Int) {
+    Glide.with(view.context)
+        .load(drawableRes)
+        .into(view)
 }
